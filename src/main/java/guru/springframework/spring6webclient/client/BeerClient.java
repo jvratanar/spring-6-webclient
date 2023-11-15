71afe58336fdd51bf462d5b589a1a03b05ee9c48
Janez Franc Vratanar <jvratanar@gmail.com>
2023-11-15 16:19:20 +0100

    Section 32, Chapter 318.

---
+++ src/main/java/guru/springframework/spring6webclient/client/BeerClient.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring6webclient.client;
+
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+public interface BeerClient {
+    Flux<String> listBeer();
+    Flux<Map> listBeerMap();
+}
---
+++ src/test/java/guru/springframework/spring6webclient/client/BeerClientImplTest.java
@@ -0,0 +1,40 @@
+package guru.springframework.spring6webclient.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerClientImplTest {
+    @Autowired
+    BeerClient client;
+
+    @Test
+    void testGetMap() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        this.client.listBeerMap().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
+    }
+
+    @Test
+    void listBeer() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        this.client.listBeer().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
+    }
+}
